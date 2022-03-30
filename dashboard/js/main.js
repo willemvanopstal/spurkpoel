8ebe3981dde3fb5c18cb59ae4eabf037daee1189
willemvanopstal <23736201+willemvanopstal@users.noreply.github.com>
2022-03-30 18:40:38 +0200

    nicer chart

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -53,77 +53,166 @@ $(function() {
 	animate();
 })
 
-
-
-
-	Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/new-intraday.json', function (data) {
-
-	    // create the chart
-	    Highcharts.stockChart('graph', {
+var options = {
 
 				chart: {
-					backgroundColor: '#fff6f0',
-					marginLeft: 22,
-					marginRight: 22
+						renderTo: 'graph',
+						backgroundColor: '#fff6f0',
+						marginLeft: 22,
+						marginRight: 22,
+						style: {
+						 fontFamily: "'Ubuntu Mono', sans-serif",
+							fontSize: "15px"
+						}
+	      },
+
+				rangeSelector: {
+						buttons: [{
+								type: 'hour',
+								count: 12,
+								text: '12h'
+						}, {
+								type: 'hour',
+								count: 24,
+								text: '24h'
+						}, {
+								type: 'hour',
+								count: 168,
+								text: '7d'
+						}, {
+								type: 'day',
+								count: 30,
+								text: '30d'
+						}, {
+								type: 'all',
+								count: 1,
+								text: 'All'
+						}],
+						selected: 1,
+						inputEnabled: true
 				},
 
-	        xAxis: {
-	            gapGridLineWidth: 0
-	        },
+				yAxis: [{
+					 labels: {
+							 align: 'right',
+							 x: -3
+					 },
+					 title: {
+							 text: 'Waterstand'
+					 },
+					 height: '54%',
+					 lineWidth: 2,
+					 resize: {
+							 enabled: true
+					 },
+					 opposite: true,
+					 offset: -80
+			 }, {
+					 labels: {
+							 align: 'right',
+							 x: -3
+					 },
+					 title: {
+							 text: 'Druk'
+					 },
+					 top: '59%',
+					 height: '18%',
+					 offset: 0,
+					 lineWidth: 2
+			 }, {
+					 labels: {
+							 align: 'right',
+							 x: -3
+					 },
+					 gridLineWidth: 0,
+					 title: {
+							 text: 'Temperatuur'
+					 },
+					 // top: '82%',
+					 height: '54%',
+					 offset: 0,
+					 lineWidth: 2
+			 }, {
+					 labels: {
+							 align: 'right',
+							 x: -3
+					 },
+					 title: {
+							 text: 'Neerslag'
+					 },
+					 top: '82%',
+					 height: '18%',
+					 offset: 0,
+					 lineWidth: 2
+			 }],
 
-	        rangeSelector: {
-	            buttons: [{
-	                type: 'hour',
-	                count: 12,
-	                text: '12h'
-	            }, {
-	                type: 'hour',
-	                count: 24,
-	                text: '24h'
-	            }, {
-	                type: 'hour',
-	                count: 168,
-	                text: '7d'
-	            }, {
-	                type: 'day',
-	                count: 30,
-	                text: '30d'
-	            }, {
-	                type: 'all',
-	                count: 1,
-	                text: 'All'
-	            }],
-	            selected: 1,
-	            inputEnabled: true
-	        },
+        series: [{
+            name: 'Waterstand',
+            data: null,
+            type: 'areaspline',
+            threshold: null,
+						color: '85aab2',
+            tooltip: {
+                valueDecimals: 2
+            },
+            fillColor: '#85aab2',
+						//  {
+            //     linearGradient: {
+            //         x1: 0,
+            //         y1: 0,
+            //         x2: 0,
+            //         y2: 1
+            //     },
+            //     stops: [
+						// 			[0, '#85aab2' ],
+						// 			[1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+            //     ]
+            // }
+        },{
+            type: 'spline',
+            name: 'Druk',
+            data: null,
+            yAxis: 1
+			},{
+					type: 'spline',
+					name: 'Temperatuur',
+					data: null,
+					yAxis: 2
+		},{
+				type: 'column',
+				name: 'Neerslag',
+				data: null,
+				yAxis: 3
+	}],
 
-					exporting: {
-						enabled: false,
-					},
+	plotOptions: {
+        series: {
+            // general options for all series
+        },
+        column: {
+            color: '#85aab2',
+						maxPointWidth: 10
+        }
+    },
+    }
 
-	        series: [{
-	            name: 'AAPL',
-	            type: 'area',
-	            data: data,
-	            gapSize: 5,
-							color: '85aab2',
-	            tooltip: {
-	                valueDecimals: 2
-	            },
-	            fillColor: {
-	                linearGradient: {
-	                    x1: 0,
-	                    y1: 0,
-	                    x2: 0,
-	                    y2: 1
-	                },
-	                stops: [
-										// [0, Highcharts.getOptions().colors[0]],
-										[0, '#85aab2' ],
-	                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-	                ]
-	            },
-	            threshold: null
-	        }]
-	    });
-	});
+
+$.getJSON('http://localhost:8000/webserver/spurkpoel_2.json', function(json) {
+	console.log(json)
+        val1 = [];
+				val2 = [];
+				val3 = [];
+				val4 = [];
+        $.each(json, function(key,value) {
+					val1.push([value[0], value[1]]);
+					val2.push([value[0], value[2]]);
+					val3.push([value[0], value[3]]);
+					val4.push([value[0], value[3]]);
+        });
+
+        options.series[0].data = val1;
+				options.series[1].data = val2;
+				options.series[2].data = val3;
+				options.series[3].data = val4;
+        chart = new Highcharts.stockChart(options);
+     });
