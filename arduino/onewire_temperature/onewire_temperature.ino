6fbc1c30e1e19d137106e0098000b27a2f97e022
willemvanopstal <willemvanopstal@users.noreply.github.com>
2022-03-31 10:04:05 +0200

    working node with payload

--- arduino/onewire_temperature/onewire_temperature.ino
+++ arduino/onewire_temperature/onewire_temperature.ino
@@ -2,10 +2,19 @@
 #include <DallasTemperature.h>
 
 float temp = 0.0;
-int oneWireBus = 12;
+int oneWireBus = 5;
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 
+ /*
+          0PSI = 0.5V, 10PSI = 4.5V
+          0bar = 0.5V, 0.689476bar = 4.5V
+          0mbar = (1023 / 10) = 102.3 = 102b
+          689.476mbar = 1023 - (1023 / 10) = 921.7 = 922b
+          mbar/bit = 689.476 / (922 - 102) = 0.84082439 mbar/bit
+      
+          mmH20 = mbar * 10.19716
+        */
 
  void setup() {
     Serial.begin(9600);
